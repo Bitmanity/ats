52d10cc2840c3896076e085983425149755993a7
Jatin Parmar <jatinparmar96@gmail.com>
2018-07-03 11:32:53 +0530

    Riased Limit to 75 and removed time constraint

--- app/Http/Controllers/GpsController.php
+++ app/Http/Controllers/GpsController.php
@@ -20,9 +20,8 @@ class GpsController extends Controller
 
     public function sendCoordinates(Request $request, $imei)
     {
-        $startTime = Carbon::now()->subRealHour(7)->toDateTimeString();
-        $endTime = Carbon::now()->toDateTimeString();
-        $coordinates = LiveData::where('imei', $imei)->whereBetween('created_at', [$startTime, $endTime])->latest()->limit(50)->get();
+
+        $coordinates = LiveData::where('imei', $imei)->latest()->limit(75)->get();
         $coordinates = json_encode($coordinates);
         return ($coordinates);
     }
